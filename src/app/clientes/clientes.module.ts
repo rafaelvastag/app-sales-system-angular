d658063b58f24754b30c17e2c2544a5fd58b537f
Rafael Vastag <rgvastag@gmail.com>
2021-01-05 14:04:37 -0300

    Criacao navegacao formulario Cliente

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientesRoutingModule } from './clientes-routing.module';
-
+import { ClientesFormComponent } from './clientes-form/clientes-form.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ClientesRoutingModule
-  ]
+  declarations: [ClientesFormComponent],
+  imports: [CommonModule, ClientesRoutingModule],
+  exports: [ClientesFormComponent],
 })
-export class ClientesModule { }
+export class ClientesModule {}
