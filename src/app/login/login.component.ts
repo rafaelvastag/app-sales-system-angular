1f21f811a97300d89605afe1eee1d28844f2c91c
Rafael Vastag <rgvastag@gmail.com>
2021-01-19 16:18:33 -0300

    Tela de login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  username: string;
+  password: string;
+  loginError: boolean;
+  cadastrando: boolean;
+
+  constructor(private router: Router) { }
+
+  onSubmit() {
+    this.router.navigate(['/home']);
+  }
+
+  prepararCadastrar(event) {
+    event.preventDefault();
+    this.cadastrando = true;
+  }
+
+  cancelarCadastro(){
+    this.cadastrando = false;
+  }
+
+}
